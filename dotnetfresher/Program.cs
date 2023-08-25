860b506bc787fd557823bacfd7a8be8709c3edd1
sona-user-369 <donadavak@gmail.com>
2023-08-25 21:33:49 +0100

    initial commit

---
+++ dotnetfresher/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+var name = Console.ReadLine() ;
+var currenDate = DateTime.Now ;
+Console.WriteLine($"{Environment.NewLine} user {name} on {currenDate:d} at {currenDate:t}");
+Console.WriteLine($"{Environment.NewLine} press any key to exit");
+Console.ReadKey(true);
